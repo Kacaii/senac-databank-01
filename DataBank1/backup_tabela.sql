b5359ffdc058e92b2a7fd3831285836570dc1764
KacaiiSENAC <kacaii.privado@gmail.com>
2024-10-03 09:24:07 -0300

    formated backup sql file

--- DataBank1/backup_tabela.sql
+++ DataBank1/backup_tabela.sql
@@ -1,18 +1,20 @@
 -- Criando Tabelas
 
 CREATE TABLE departamentos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER,
     nome VARCHAR(255),
-    bloco VARCHAR(40)
+    bloco VARCHAR(40),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE empregados (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER,
     nome VARCHAR(255),
     salario INT,
     matricula INT,
     data_nascimento DATE,
     id_departamento INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (id_departamento) REFERENCES departamentos (id)
 );
 
@@ -33,19 +35,41 @@ INSERT INTO empregados (
     id_departamento
 )
 VALUES
-('Tamires Oliveira', 1024, 357, '1998-01-07', 04),
-('Roberta Miranda', 3065, 357, '2001-01-07', 1),
-('Cáscio Silva', 5365, 147, '1999-08-01', 4),
+('Tamires Oliveira', 1024, 357, '1998-01-07', 4),
+('Roberta Miranda', 3065, 357, '2001-01-07', 2),
+('Cáscio Silva', 5365, 147, '1999-08-01', 3),
 ('Maria Silva', 7824, 357, '2010-07-10', 1);
 
--- Transacoes
+-- Retorna a quantidade total dos empregados por departamento
 
-START TRANSACTION;
+SELECT
+    id_departamento,
+    ROUND(AVG(salario)) AS media_salarial
+FROM empregados
+GROUP BY id_departamento;
 
-UPDATE empregados
-SET
-    salario = salario * 1.1
-WHERE
-    id_departamento = 1;
+-- Lista o total de empregados na empresa
 
-COMMIT;
+SELECT
+    COUNT(id)
+        AS total_empregados
+FROM empregados; --  
+
+-- Lista a media salarial, o menor e o maior salario
+
+SELECT
+    ROUND(AVG(salario)) AS media_salarial,
+    ROUND(MIN(salario)) AS menor_salario,
+    ROUND(MAX(salario)) AS maior_salario
+FROM empregados;
+
+-- Lista cada departamento com o total de salario pago
+
+SELECT
+    id_departamento,
+    SUM(salario)
+        AS salario_por_departamento
+FROM
+    empregados
+GROUP BY
+    id_departamento;
