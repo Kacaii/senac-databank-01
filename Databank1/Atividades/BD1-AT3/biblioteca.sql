39a2bcf5b5c0cfb94985adebf9c2e97e1cd49420
KacaiiSENAC <kacaii.privado@gmail.com>
2024-11-14 11:10:30 -0300

    More Progress

--- Databank1/Atividades/BD1-AT3/biblioteca.sql
+++ Databank1/Atividades/BD1-AT3/biblioteca.sql
@@ -1,3 +1,7 @@
+-- =====================
+-- == TABELA ORIGINAL ==
+-- =====================
+
 CREATE TABLE biblioteca (
     id_livro INT,
     titulo VARCHAR(100),
@@ -18,9 +22,12 @@ CREATE TABLE biblioteca (
 -- == TABELAS NORMALIZADAS ==
 -- ==========================
 
+-- TODO: Terminar depois e enviar ate 2024-11-14 17:00
+
 CREATE TABLE categorias (
     id INTERGER,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    nome VARCHAR(100)
 );
 
 -- Tabela associativa
@@ -33,7 +40,11 @@ CREATE TABLE livro_autor (
 
 CREATE TABLE alunos ();
 
-CREATE TABLE editoras ();
+CREATE TABLE editoras (
+    id INTERGER,
+    nome VARCHAR(100),
+    cidade_editora VARCHAR(100)
+);
 
 CREATE TABLE autores (
     id_autor INTERGER,
@@ -51,5 +62,6 @@ CREATE TABLE livros (
     autor VARCHAR(100),
     FOREIGN KEY (autor) REFERENCES autores (id),
     editora VARCHAR(100),
-    FOREIGN KEY (editora) REFERENCES editoras (id)
+    FOREIGN KEY (editora) REFERENCES editoras (id),
+    isbn INT
 );
