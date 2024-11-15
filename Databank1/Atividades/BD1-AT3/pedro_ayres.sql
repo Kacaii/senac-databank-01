9e37df3f7f6637a37e2ab78e21aa7a1529f99a46
Kacaii <kacaii.privado@gmail.com>
2024-11-15 08:39:35 -0300

    Added `.db` file for Queries

--- Databank1/Atividades/BD1-AT3/pedro_ayres.sql
+++ Databank1/Atividades/BD1-AT3/pedro_ayres.sql
@@ -2,74 +2,84 @@
 -- == TABELA ORIGINAL ==
 -- =====================
 
-CREATE TABLE biblioteca (
-    id_livro INT,
-    titulo VARCHAR(100),
-    autor VARCHAR(100),
-    categoria VARCHAR(100),
-    ano_pubi YEAR,
-    editora VARCHAR(100),
-    cidade_editora VARCHAR(100),
-    isbn INT,
-    id_aluno VARCHAR(100),
-    nome_aluno VARCHAR(100),
-    data_emprestimo DATE,
-    date_devolucao DATE
-);
+-- CREATE TABLE IF NOT EXISTS biblioteca (
+--     id_livro INT,
+--     titulo VARCHAR(100),
+--     autor VARCHAR(100),
+--     categoria VARCHAR(100),
+--     ano_pubi YEAR,
+--     editora VARCHAR(100),
+--     cidade_editora VARCHAR(100),
+--     isbn INT,
+--     id_aluno VARCHAR(100),
+--     nome_aluno VARCHAR(100),
+--     data_emprestimo DATE,
+--     date_devolucao DATE
+-- );
 
 -- ==========================
 -- == ATIVIDADE 2024-11-14 ==
 -- == TABELAS NORMALIZADAS ==
 -- ==========================
 
-CREATE TABLE categorias (
+CREATE TABLE IF NOT EXISTS categorias (
     id INTEGER PRIMARY KEY,
-    nome TEXT
+    nome TEXT NOT NULL
 );
 
-CREATE TABLE alunos (
+CREATE TABLE IF NOT EXISTS alunos (
     id INTEGER PRIMARY KEY,
-    nome TEXT
+    nome TEXT NOT NULL
 );
 
-CREATE TABLE editoras (
+CREATE TABLE IF NOT EXISTS editoras (
     id INTEGER PRIMARY KEY,
-    nome TEXT,
+    nome TEXT NOT NULL,
     cidade_editora TEXT
 );
 
 
-CREATE TABLE autores (
+CREATE TABLE IF NOT EXISTS autores (
     id INTEGER PRIMARY KEY,
-    nome TEXT,
+    nome TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS livros (
+    id INTEGER PRIMARY KEY,
+    titulo TEXT NOT NULL,
+    ano_publi INT,
     id_editora INT,
+    isbn INT UNIQUE,
     FOREIGN KEY (id_editora) REFERENCES editoras (id)
 );
 
-CREATE TABLE livros (
+-- Tabela associativa
+CREATE TABLE IF NOT EXISTS livro_autor (
     id INTEGER PRIMARY KEY,
-    titulo TEXT,
-    ano_publi INT,
-    id_autor INT,
+    id_livro INT NOT NULL,
+    id_autor INT NOT NULL,
+    FOREIGN KEY (id_livro) REFERENCES livros (id),
     FOREIGN KEY (id_autor) REFERENCES autores (id),
-    id_editora INT,
-    FOREIGN KEY (id_editora) REFERENCES editoras (id),
-    isbn INT
+    UNIQUE (id_livro, id_autor)
 );
 
 -- Tabela associativa
-CREATE TABLE livro_autor (
+CREATE TABLE IF NOT EXISTS autor_editora (
     id INTEGER PRIMARY KEY,
-    id_livro INT,
-    id_autor INT,
-    FOREIGN KEY (id_livro) REFERENCES livros (id),
-    FOREIGN KEY (id_autor) REFERENCES autores (id)
+    id_autor INT NOT NULL,
+    id_editora INT NOT NULL,
+    FOREIGN KEY (id_autor) REFERENCES autores (id),
+    FOREIGN KEY (id_editora) REFERENCES editoras (id),
+    UNIQUE (id_autor, id_editora)
 );
 
-CREATE TABLE emprestimos (
+CREATE TABLE IF NOT EXISTS emprestimos (
     id INTEGER PRIMARY KEY,
-    data_emprestimo DATE,
+    id_aluno INTEGER NOT NULL,
+    id_livro INT NOT NULL,
+    data_emprestimo DATE NOT NULL,
     data_devolucao DATE,
-    id_aluno INTEGER,
-    FOREIGN KEY (id_aluno) REFERENCES alunos (id)
+    FOREIGN KEY (id_aluno) REFERENCES alunos (id),
+    FOREIGN KEY (id_livro) REFERENCES livros (id),
+    UNIQUE (id_livro, data_devolucao)
 );
